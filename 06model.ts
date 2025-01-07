50b5a881ebb0ae10469e6f488fb38d0be688e0a4
mf-toshiyuki-yamamoto <toshiyuki_yamamoto@mforce.co.jp>
2025-01-07 17:03:03 +0900

    確認お願いします。

---
+++ 06model.ts
@@ -0,0 +1,7 @@
+function valueInputs(value1: number | string, value2: number | string){
+    if (typeof value1 === 'number' && typeof value2 === 'number') {
+        return value1 * value2;
+    } else {
+        return value1.toString() + value2.toString();
+    }
+}
