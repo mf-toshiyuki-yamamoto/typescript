3258db72eeeb589abd6f4e33bc674b5f761830ea
mf-toshiyuki-yamamoto <toshiyuki_yamamoto@mforce.co.jp>
2025-01-07 15:37:20 +0900

    interfaceの命名・interfaceの継承部分の修正

--- 05interface.ts
+++ 05interface.ts
@@ -1,19 +1,19 @@
 /*名前をプロパティに持つ人間インターフェースを作成*/
-interface HumanInterface{
+interface Human{
     name:string;
 }
 
 
 /*人間インターフェースを継承し、年齢をプロパティに持つスポーツ選手インターフェースを作成*/
-interface HumanInterface extends AthleteInterface{
+interface Athlete extends Human{
 	age:number
 }
 
 
 /*スポーツ選手インターフェースを実装した、サッカー選手クラスを作成してください。*/
-class SoccerPlayer implements HumanInterface,AthleteInterface {
+class SoccerPlayer implements Athlete,Human {
     name : 'lionelMessi';
-    age  : '37';
+    age  : 37;
 }
 
 
