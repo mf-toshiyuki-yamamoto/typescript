0793a2b62b1c6bede7e599269d1de5e73f62836c
mf-toshiyuki-yamamoto <toshiyuki_yamamoto@mforce.co.jp>
2025-01-07 14:19:46 +0900

    03fizzbuzz

--- 03Fizz.ts
+++ 03Fizz.ts
@@ -1,12 +1,16 @@
 const FizzBuzz = (n: number): void => {
     for (let i = 1; i <= 100; i++) {
-      if (i % 3 === 0) {
+      if (i % 15 === 0) {
+        console.log('FizzBuzz');
+        }
+        else if (i % 3 === 0){
         console.log('Fizz');
-      } else {
+        }
+        else if (i % 5 === 0){
+        console.log('Buzz');        
+        }else {
         console.log(i);
-      }
+        }
     }
   };
- 
-//100まで表示する
   FizzBuzz(100);
