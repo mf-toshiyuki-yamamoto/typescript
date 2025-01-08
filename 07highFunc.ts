ddc7f56e3b28080b5e587a74a384b9cd393cd5c6
mf-toshiyuki-yamamoto <toshiyuki_yamamoto@mforce.co.jp>
2025-01-08 11:35:54 +0900

    レビュー修正

--- 07highFunc.ts
+++ 07highFunc.ts
@@ -1,6 +1,8 @@
+
 // 商品の価格（税抜き）
 const excludingTax: number[] = [300, 100, 120, 1000, 500];
 
+/*
 // 消費税率は8%とする
 const taxRate: number = 0.08;
 
@@ -10,3 +12,7 @@ const calculateIncludingTax = (price: number): number => {return price * (1 + ta
 // map関数を使用
 const pricesWithTax: number[] = excludingTax.map(calculateIncludingTax);
 console.log(pricesWithTax);
+*/
+
+const pricesIncludingTax = excludingTax.map(price => price *(1 + 0.08));
+console.log(pricesIncludingTax);
